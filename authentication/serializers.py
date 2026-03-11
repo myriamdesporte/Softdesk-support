d42b81252621396d5f4fb03c808ead3eb7fd6e4d
Myriam Desporte <myriam.desporte@gmail.com>
2026-03-11 18:06:10 +0100

    Feat: enforce minimum age of 15 in UserSerializer

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from rest_framework import serializers
 from .models import User
 
@@ -6,6 +8,22 @@ class UserSerializer(serializers.ModelSerializer):
     """
     Serializer for User model.
     """
+
+    def validate_date_of_birth(self, value):
+        today = date.today()
+        age = today.year - value.year - (
+            (today.month, today.day) < (value.month, value.day)
+        )
+
+        if age < 15:
+            raise serializers.ValidationError(
+                "User must be at least 15 years old."
+            )
+
+        return value
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
 
     class Meta:
         model = User
