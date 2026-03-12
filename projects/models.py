87aff3011e70dc053fd2dcf0ad6cc69c9ae4c65f
Myriam Desporte <myriam.desporte@gmail.com>
2026-03-12 14:47:38 +0100

    Add Project model with name, description, type, author and created_time

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Project(models.Model):
+    """
+    Software project.
+    """
+    TYPE_CHOICES = [
+        ('back-end', 'Back-end'),
+        ('front-end', 'Front-end'),
+        ('iOS', 'iOS'),
+        ('Android', 'Android'),
+    ]
+
+    name = models.CharField(max_length=128)
+    description = models.TextField
+    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='authored_projects'
+    )
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
