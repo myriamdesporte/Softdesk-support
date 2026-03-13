11577bf8f738a8cd7796af415fdc630c5132513d
Myriam Desporte <myriam.desporte@gmail.com>
2026-03-13 13:19:48 +0100

    Add viewsets for Project and Contributor models

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from projects.models import Project, Contributor
+from projects.serializers import ProjectSerializer, ContributorSerializer
+
+
+class ProjectViewSet(ModelViewSet):
+    """ViewSet for Project model."""
+    serializer_class = ProjectSerializer
+
+    def get_queryset(self):
+        return Project.objects.all()
+
+
+class ContributorViewSet(ModelViewSet):
+    """
+    ViewSet for Contributor model.
+    """
+    serializer_class = ContributorSerializer
+
+    def get_queryset(self):
+        return Contributor.objects.all()
