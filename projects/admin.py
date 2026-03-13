4aa55ec929b2053c2cd68ef823f9f4527782c8f3
Myriam Desporte <myriam.desporte@gmail.com>
2026-03-13 11:33:18 +0100

    Register Project and Contributor models in admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Project, Contributor
 
-# Register your models here.
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    """Admin for Project model."""
+    list_display = ('name', 'type', 'author', 'created_time')
+    list_filter = ('type', 'created_time')
+    search_fields = ('name', 'description')
+    readonly_fields = ('created_time',)
+
+
+@admin.register(Contributor)
+class ContributorAdmin(admin.ModelAdmin):
+    """Admin for Contributor model."""
+    list_display = ('user', 'project', 'created_time')
+    list_filter = ('created_time',)
+    search_fields = ('user__username', 'project__name')
+    readonly_fields = ('created_time',)
