963801ed2150e80cde3b49fec0a87e35ba130eea
Myriam Desporte <myriam.desporte@gmail.com>
2026-03-20 15:31:05 +0100

    Add serializers for Issue and Comment models

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from projects.models import Project, Contributor
+from projects.models import Project, Contributor, Issue, Comment
 
 
 class ProjectSerializer(serializers.ModelSerializer):
@@ -35,3 +35,44 @@ class ContributorSerializer(serializers.ModelSerializer):
             'created_time',
         ]
         read_only_fields = ['id', 'created_time']
+
+
+class IssueSerializer(serializers.ModelSerializer):
+    """Serializer for Issue model."""
+    author = serializers.ReadOnlyField(source='author.username')
+    assignee_username = serializers.ReadOnlyField(source='assignee.username')
+    project_name = serializers.ReadOnlyField(source='project.name')
+
+    class Meta:
+        model = Issue
+        fields = [
+            'id', 'title', 'description', 'priority', 'tag', 'status',
+            'project', 'project_name', 'author', 'assignee', 'assignee_username',
+            'created_time'
+        ]
+        read_only_fields = ['id', 'author', 'project', 'created_time']
+
+    def validate_assignee(self, value):
+        """Validate that assignee is a contributor of the project."""
+        if value:
+            project = self.initial_data.get('project') if self.initial_data else None
+            if not project and self.instance:
+                project = self.instance.project.id
+
+            if project and not Contributor.objects.filter(project_id=project, user=value).exists():
+                raise serializers.ValidationError(
+                    "Assignee must be a contributor of this project."
+                )
+
+        return value
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """Serializer for Comment model."""
+    author = serializers.ReadOnlyField(source='author.username')
+    issue_title = serializers.ReadOnlyField(source='issue.title')
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'uuid', 'description', 'author', 'issue', 'issue_title', 'created_time']
+        read_only_fields = ['id', 'uuid', 'author', 'issue', 'created_time']
