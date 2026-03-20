ffa17ec37400834191dd1d9de973a232ac9506a8
Myriam Desporte <myriam.desporte@gmail.com>
2026-03-20 15:34:15 +0100

    Add viewsets for Issue and Comment with nested filtering

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 
-from projects.models import Project, Contributor
-from projects.serializers import ProjectSerializer, ContributorSerializer
+from projects.models import Project, Contributor, Issue, Comment
+from projects.serializers import ProjectSerializer, ContributorSerializer, IssueSerializer, CommentSerializer
 
 
 class ProjectViewSet(ModelViewSet):
@@ -38,3 +38,43 @@ class ContributorViewSet(ModelViewSet):
             serializer.save(project_id=project_id)
         else:
             serializer.save()
+
+
+class IssueViewSet(ModelViewSet):
+    """ViewSet for Issue model."""
+    serializer_class = IssueSerializer
+
+    def get_queryset(self):
+        """Return only issues belonging to the project in the URL."""
+        project_id = self.kwargs.get('project_pk')
+        if project_id:
+            return Issue.objects.filter(project_id=project_id)
+        return Issue.objects.all()
+
+    def perform_create(self, serializer):
+        """Set the author and project when creating an issue."""
+        project_id = self.kwargs.get('project_pk')
+        if project_id:
+            serializer.save(author=self.request.user, project_id=project_id)
+        else:
+            serializer.save(author=self.request.user)
+
+
+class CommentViewSet(ModelViewSet):
+    """ViewSet for Comment model."""
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        """Return only comments belonging to the issue in the URL"""
+        issue_id = self.kwargs.get('issue_pk')
+        if issue_id:
+            return Comment.objects.filter(issue_id=issue_id)
+        return Comment.objects.all()
+
+    def perform_create(self, serializer):
+        """Set the author and issue when creating a comment."""
+        issue_id = self.kwargs.get('issue_pk')
+        if issue_id:
+            serializer.save(author=self.request.user, issue_id=issue_id)
+        else:
+            serializer.save(author=self.request.user)
