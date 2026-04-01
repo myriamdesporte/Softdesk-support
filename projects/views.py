1a3b17ff37fa303b3603d1a7f392252feb134e87
Myriam Desporte <myriam.desporte@gmail.com>
2026-04-01 18:09:48 +0200

    Add pagination: 5 items per page

--- projects/views.py
+++ projects/views.py
@@ -18,7 +18,7 @@ class ProjectViewSet(ModelViewSet):
 
     def get_queryset(self):
         """Return only projects where user is a contributor or author."""
-        return Project.objects.filter(contributors__user=self.request.user).distinct()
+        return Project.objects.filter(contributors__user=self.request.user).select_related('author').distinct()
 
     def perform_create(self, serializer):
         """
@@ -39,7 +39,7 @@ class ContributorViewSet(ModelViewSet):
         """Filter contributors by project."""
         project_id = self.kwargs.get("project_pk")
         if project_id:
-            return Contributor.objects.filter(project_id=project_id)
+            return Contributor.objects.filter(project_id=project_id).select_related('user', 'project')
         return Contributor.objects.none()
 
     def perform_create(self, serializer):
@@ -59,7 +59,7 @@ class IssueViewSet(ModelViewSet):
         """Filter issues by project."""
         project_id = self.kwargs.get("project_pk")
         if project_id:
-            return Issue.objects.filter(project_id=project_id)
+            return Issue.objects.filter(project_id=project_id).select_related('author', 'assignee', 'project')
         return Issue.objects.none()
 
     def perform_create(self, serializer):
@@ -79,7 +79,7 @@ class CommentViewSet(ModelViewSet):
         """Filter comments by issue."""
         issue_id = self.kwargs.get("issue_pk")
         if issue_id:
-            return Comment.objects.filter(issue_id=issue_id)
+            return Comment.objects.filter(issue_id=issue_id).select_related('author', 'issue')
         return Comment.objects.none()
 
     def perform_create(self, serializer):
